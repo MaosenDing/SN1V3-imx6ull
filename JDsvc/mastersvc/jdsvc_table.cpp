04d142e235acd00c2e1eca46a4212e80c6a20974
ririyeye <200610237@qq.com>
2019-12-02 11:26:58 +0800

    add jd load tim table

--- JDsvc/mastersvc/jdsvc_table.cpp
+++ JDsvc/mastersvc/jdsvc_table.cpp
@@ -3,25 +3,38 @@
 #include "svc.h"
 #include <thread>
 #include "SN1V2_com.h"
+#include <mutex>
+#include <iostream>
+
 using namespace std;
-
-
-
 
 
 struct jdtablesvc :public JDAUTOSEND {
 	jdtablesvc()
 	{
+		scan_file(this, force_time_table_save_path);
 		thread watch(SetWatchFile, force_time_table_save_path, scan_file, this);
 		watch.detach();
 	}
+	std::mutex tableLock;
+	std::vector<timTableSet> timeset;
 
 	static void scan_file(void * p, const char * fil)
 	{
+		if (!p || !fil) {
+			return;
+		}
+		
+
 		jdtablesvc *thissvc = (jdtablesvc*)p;
-
-
-
+		std::unique_lock<std::mutex> lk(thissvc->tableLock);
+		thissvc->timeset.clear();
+		ERR_STA err;
+		if ((err = load_table(force_time_table_save_path, thissvc->timeset)) != err_ok) {
+			cout << "fail to load " << endl;
+		} else {
+			cout << "succ to load ,size = " << thissvc->timeset.size() << endl;
+		}
 	}
 
 
