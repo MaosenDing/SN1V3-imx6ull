e9589e268c14cdaa8a9cdd1890b9d1a5392b6916
ririyeye <200610237@qq.com>
2019-12-03 14:23:11 +0800

    set lib to object

--- compiler.cmake
+++ compiler.cmake
@@ -5,4 +5,5 @@ SET(toolpathprefix /share/nuc/buildroot-2019.02.1/output/host/bin/arm-buildroot-
 SET(CMAKE_C_COMPILER ${toolpathprefix}gcc)
 SET(CMAKE_CXX_COMPILER ${toolpathprefix}g++)
 SET(CMAKE_STRIP  ${toolpathprefix}strip)
+SET(CMAKE_AR ${toolpathprefix}ar)
 
