5b5532b8068bc89994fafc6ece04c1bb0614f7c5
ririyeye <200610237@qq.com>
2020-04-10 16:35:11 +0800

    添加部分参数 添加参数在表中序号

--- com_inc/sn1v3cfg.h
+++ com_inc/sn1v3cfg.h
@@ -7,7 +7,10 @@ extern "C" {
 #endif
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stddef.h>
+
+	//定义和类型参考《LOC无线定日镜控制系统设计要求》
 
 	enum dateType
 	{
@@ -18,6 +21,7 @@ extern "C" {
 
 		FLOAT32,
 		DOUBLE64,
+
 		INT32,
 		LONG64,
 
@@ -25,6 +29,7 @@ extern "C" {
 		MAC,
 
 		BOOLTYPE,
+		IP,
 	};
 
 	typedef struct {
@@ -32,19 +37,28 @@ extern "C" {
 		size_t diff;
 		dateType typ;
 		void(*default_value)(void *);
+		size_t SeqInTable;
 	}CFG_INFO;
 
-#define Set_data(table,member,typ,def_method)    {#member,offsetof(table,member),typ,def_method}
-	
+#define Set_data(table,member,typ,def_method,seq)    {#member,offsetof(table,member),typ,def_method,seq}
+
 	typedef struct {
 		char HeliostatID[16];
 		char ReducerM[16];
 		char PutterM[16];
 		char DriveM[16];
-		char SensorM[16];
+		char FWSensorM[16];
+		char FYSensorM[16];
 		char InstallTime[16];
-		char SoftwareVersion[64];
-		char mac[6];
+		char PSN[6];
+		char MDC2_SV_1[16];
+		char MDC2_SV_2[16];
+		char MDC2_SV_3[16];
+		char MDC2_SV_4[16];
+		char MDC2_SV_5[16];
+		char DRI1_SV[16];
+		char DRI2_SV[16];
+		char Wiresless_SV[16];
 		char HeliostatType[16];
 		char SN2ID[16];
 		float latitude;
@@ -57,12 +71,23 @@ extern "C" {
 		float HeliostatPointY;
 		float HeliostatPointZ;
 	}T1_table;
-#define T1_Set_data(member,typ,def_method)      Set_data(T1_table,member,typ,def_method)
+
 
 	typedef struct {
-		float initSpeedx;
+		int32_t FWStartSpeed;
+		int32_t FWMaxSpeed;
+		int32_t FYStartSpeed;
+		int32_t FYMaxSpeed;
+		int32_t FWReductRt;
+		int32_t PutterReductRt;
+		int32_t PutterScrewPara;
+		int32_t PutterMaxLen;
+		int32_t PutterPara4;
+		int32_t ServoControlCycle;
+		int32_t CompesatPara;
+		int32_t MaxMotorCurr;
 	}T2_table;
-#define T2_Set_data(member,typ,def_method)      Set_data(T2_table,member,typ,def_method)
+
 
 
 	typedef struct {
@@ -111,18 +136,67 @@ extern "C" {
 		float OLW_e3;
 		int OLW_e4;
 	}T3_table;
-#define T3_Set_data(member,typ,def_method)      Set_data(T3_table,member,typ,def_method)
+
 
 	typedef struct {
-		char AP1[32];
+		char M_AP1[32];
+		char B_AP2[32];
+		char D_AP0[32];
+		int32_t CM_Group;
+		int32_t CM_Times;
+		int32_t CM_Cycle;
+		int32_t AP_SS;
+		int32_t Baudrate;
+		int32_t TransmitPower;
+		char ServerIP[4];
+		int32_t ServerPort;
+		char LocalIP[4];
+		int32_t Channel;
+		int32_t SinglePacketMax;
 	}T4_table;
-#define T4_Set_data(member,typ,def_method)      Set_data(T4_table,member,typ,def_method)
+
 
 	typedef struct {
-		int SN1_P1;
-	}T6_table;
-#define T6_Set_data(member,typ,def_method)      Set_data(T6_table,member,typ,def_method)
+		int32_t SN1_P1;//行像素
+		int32_t SN1_P2;//列像素
+		float SN1_P3;//像元大小
+		float SN1_P4;//焦距
+		int32_t SN1_P5;//增益
+		int32_t SN1_P6;//曝光
+		int32_t SN1_P7;//水平翻转 true/false
+		int32_t SN1_P8;//竖直翻转 true/false
+		int32_t SN1_P9;//灰度阈值
+		float SN1_P10;//二值化阈值
+		float SN1_P11;//圆度阈值
+		float SN1_P12;//上午有效数据百分比阈值
+		float SN1_P13;//下午有效数据百分比阈值
+		float SN1_P14;//图像采集用 太阳高度角阈值？
+		int32_t SN1_P15;//主动修正标志位？ true/false
+		int32_t SN1_P16;//校正标志位 true/false
 
+		int32_t SN1_P17;//采集间隔时间
+		float SN1_P18;//《=1mrad占比
+		float SN1_P19;//《=2mrad占比
+		float SN2_P1;//目标sn3相机坐标x
+		float SN2_P2;//目标sn3相机坐标y
+		float SN2_P3;//目标sn3相机坐标z
+
+		int32_t SN2_P4;//SN2用T8表频率(行/秒)
+		int32_t SN2_P5;//SN2用最大转角速度（度/秒）
+		int32_t SN2_P6;//SN2用法线最大扫描距离（米）
+
+		int SaveOrg; //是否保留原视图片
+		int SaveBin;//是否保留二值化图片
+		char SavePath[64];//图片保持目录
+	}T6_table;
+
+#define T1_Set_data(seq,member,typ,def_method)      Set_data(T1_table,member,typ,def_method,seq)
+#define T2_Set_data(seq,member,typ,def_method)      Set_data(T2_table,member,typ,def_method,seq)
+#define T3_Set_data(seq,member,typ,def_method)      Set_data(T3_table,member,typ,def_method,seq)
+#define T4_Set_data(seq,member,typ,def_method)      Set_data(T4_table,member,typ,def_method,seq)
+#define T6_Set_data(seq,member,typ,def_method)      Set_data(T6_table,member,typ,def_method,seq)
+
+#define setDefault(type ,val) [](void *addr){*(type*)addr = val;}
 
 	typedef struct {
 		int scanftrueFlg[5] = {0};
--- comlib/text/dateDeafult.cpp
+++ comlib/text/dateDeafult.cpp
@@ -1,11 +1,24 @@
 #include "sn1v3cfg.h"
 #include <string.h>
 #include <stdint.h>
+#include <algorithm>
+
+//定义和类型参考《LOC无线定日镜控制系统设计要求》
+
+using namespace std;
+
+
+
+
+
 void stingDefault(void * addr)
 {
 	char * pos = (char *)addr;
 	*pos = 0;
 }
+
+
+
 
 void floatDefault(void * addr)
 {
@@ -31,93 +44,186 @@ void boolDefault(void * addr)
 	*pos = 0;
 }
 
+void IPServerDefault(void * addr)
+{	
+	//服务器默认ip 192.168.50.205
+	const char serverip[4] = { 192,168,50,205};
+
+	char * pos = (char *)addr;
+	reverse_copy(serverip, serverip + 4, pos);
+}
+
+void PortServerDefault(void * addr)
+{
+	//服务器默认端口 808
+	int32_t * port = (int32_t *)addr;
+	*port = 808;
+}
+
+
+void IPLocalDefault(void * addr)
+{
+	//本地默认ip 192.168.50.111
+	const char localip[4] = { 192,168,50,111 };
+
+	char * pos = (char *)addr;
+	reverse_copy(localip, localip + 4, pos);
+}
+
+
+
 
 
 const CFG_INFO T1[] = {
-	T1_Set_data(HeliostatID,dateType::STRING16,stingDefault),
-	T1_Set_data(ReducerM,dateType::STRING16,stingDefault),
-	T1_Set_data(PutterM,dateType::STRING16,stingDefault),
-	T1_Set_data(DriveM,dateType::STRING16,stingDefault),
-	T1_Set_data(SensorM,dateType::STRING16,stingDefault),
-	T1_Set_data(InstallTime,dateType::TIM16,nullptr),
-	T1_Set_data(SoftwareVersion,dateType::STRING64,stingDefault),
-
-	T1_Set_data(mac,dateType::MAC,nullptr),
-	T1_Set_data(HeliostatType,dateType::STRING16,stingDefault),
-	T1_Set_data(SN2ID,dateType::STRING16,stingDefault),
-	T1_Set_data(latitude,dateType::FLOAT32,floatDefault),
-	T1_Set_data(longtitude,dateType::FLOAT32,floatDefault),
-	T1_Set_data(elevation,dateType::FLOAT32,floatDefault),
-	T1_Set_data(Temperature,dateType::FLOAT32,floatDefault),
-	T1_Set_data(Pressure,dateType::FLOAT32,floatDefault),
-	T1_Set_data(DeltaT,dateType::FLOAT32,floatDefault),
-	T1_Set_data(HeliostatPointX,dateType::FLOAT32,floatDefault),
-	T1_Set_data(HeliostatPointY,dateType::FLOAT32,floatDefault),
-	T1_Set_data(HeliostatPointZ,dateType::FLOAT32,floatDefault),
+	T1_Set_data(1,HeliostatID,dateType::STRING16,stingDefault),
+	T1_Set_data(2,ReducerM,dateType::STRING16,stingDefault),
+	T1_Set_data(3,PutterM,dateType::STRING16,stingDefault),
+	T1_Set_data(4,DriveM,dateType::STRING16,stingDefault),
+	T1_Set_data(5,FWSensorM,dateType::STRING16,stingDefault),
+	T1_Set_data(6,FYSensorM,dateType::STRING16,stingDefault),
+	T1_Set_data(7,InstallTime,dateType::TIM16,nullptr),
+	T1_Set_data(8,PSN,dateType::MAC,nullptr),
+	T1_Set_data(9,MDC2_SV_1,dateType::STRING16,stingDefault),
+	T1_Set_data(10,MDC2_SV_2,dateType::STRING16,stingDefault),
+	T1_Set_data(11,MDC2_SV_3,dateType::STRING16,stingDefault),
+	T1_Set_data(12,MDC2_SV_4,dateType::STRING16,stingDefault),
+	T1_Set_data(13,MDC2_SV_5,dateType::STRING16,stingDefault),
+	T1_Set_data(14,DRI1_SV,dateType::STRING16,stingDefault),
+	T1_Set_data(15,DRI2_SV,dateType::STRING16,stingDefault),
+	T1_Set_data(16,Wiresless_SV,dateType::STRING16,stingDefault),
+	T1_Set_data(17,HeliostatType,dateType::STRING16,stingDefault),
+	T1_Set_data(18,SN2ID,dateType::STRING16,stingDefault),
+	T1_Set_data(19,latitude,dateType::FLOAT32,floatDefault),
+	T1_Set_data(20,longtitude,dateType::FLOAT32,floatDefault),
+	T1_Set_data(21,elevation,dateType::FLOAT32,floatDefault),
+	T1_Set_data(22,Temperature,dateType::FLOAT32,floatDefault),
+	T1_Set_data(23,Pressure,dateType::FLOAT32,floatDefault),
+	T1_Set_data(24,DeltaT,dateType::FLOAT32,floatDefault),
+	T1_Set_data(25,HeliostatPointX,dateType::FLOAT32,floatDefault),
+	T1_Set_data(26,HeliostatPointY,dateType::FLOAT32,floatDefault),
+	T1_Set_data(27,HeliostatPointZ,dateType::FLOAT32,floatDefault),
 };
 
-
 const CFG_INFO T2[] = {
-	T2_Set_data(initSpeedx,dateType::FLOAT32,floatDefault),
+	T2_Set_data(1,FWStartSpeed,dateType::INT32,int32Default),
+	T2_Set_data(2,FWMaxSpeed,dateType::INT32,int32Default),
+	T2_Set_data(3,FYStartSpeed,dateType::INT32,int32Default),
+	T2_Set_data(4,FYMaxSpeed,dateType::INT32,int32Default),
+	T2_Set_data(5,FWReductRt,dateType::INT32,int32Default),
+	T2_Set_data(6,PutterReductRt,dateType::INT32,int32Default),
+	T2_Set_data(7,PutterScrewPara,dateType::INT32,int32Default),
+	T2_Set_data(8,PutterMaxLen,dateType::INT32,int32Default),
+	T2_Set_data(9,PutterPara4,dateType::INT32,int32Default),
+	T2_Set_data(10,ServoControlCycle,dateType::INT32,int32Default),
+	T2_Set_data(11,CompesatPara,dateType::INT32,int32Default),
+	T2_Set_data(12,MaxMotorCurr,dateType::INT32,int32Default),
 };
 
 
 
 const CFG_INFO T3[] = {
 	//sn1
-	T3_Set_data(SN1_e1,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e2,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e3,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e4,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e5,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e6,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e7,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e8,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e9,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e10,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e11,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e12,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e13,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e14,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN1_e15,dateType::BOOLTYPE,boolDefault),
+	T3_Set_data(1,SN1_e1,dateType::FLOAT32,floatDefault),
+	T3_Set_data(2,SN1_e2,dateType::FLOAT32,floatDefault),
+	T3_Set_data(3,SN1_e3,dateType::FLOAT32,floatDefault),
+	T3_Set_data(4,SN1_e4,dateType::FLOAT32,floatDefault),
+	T3_Set_data(5,SN1_e5,dateType::FLOAT32,floatDefault),
+	T3_Set_data(6,SN1_e6,dateType::FLOAT32,floatDefault),
+	T3_Set_data(7,SN1_e7,dateType::FLOAT32,floatDefault),
+	T3_Set_data(8,SN1_e8,dateType::FLOAT32,floatDefault),
+	T3_Set_data(9,SN1_e9,dateType::FLOAT32,floatDefault),
+	T3_Set_data(10,SN1_e10,dateType::FLOAT32,floatDefault),
+	T3_Set_data(11,SN1_e11,dateType::FLOAT32,floatDefault),
+	T3_Set_data(12,SN1_e12,dateType::FLOAT32,floatDefault),
+	T3_Set_data(13,SN1_e13,dateType::FLOAT32,floatDefault),
+	T3_Set_data(14,SN1_e14,dateType::FLOAT32,floatDefault),
+	T3_Set_data(15,SN1_e15,dateType::BOOLTYPE,boolDefault),
 	//sn2
-	T3_Set_data(SN2_e1,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e2,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e3,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e4,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e5,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e6,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e7,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e8,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e9,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e10,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e11,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e12,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e13,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN2_e14,dateType::FLOAT32,floatDefault),
+	T3_Set_data(16,SN2_e1,dateType::FLOAT32,floatDefault),
+	T3_Set_data(17,SN2_e2,dateType::FLOAT32,floatDefault),
+	T3_Set_data(18,SN2_e3,dateType::FLOAT32,floatDefault),
+	T3_Set_data(19,SN2_e4,dateType::FLOAT32,floatDefault),
+	T3_Set_data(20,SN2_e5,dateType::FLOAT32,floatDefault),
+	T3_Set_data(21,SN2_e6,dateType::FLOAT32,floatDefault),
+	T3_Set_data(22,SN2_e7,dateType::FLOAT32,floatDefault),
+	T3_Set_data(23,SN2_e8,dateType::FLOAT32,floatDefault),
+	T3_Set_data(24,SN2_e9,dateType::FLOAT32,floatDefault),
+	T3_Set_data(25,SN2_e10,dateType::FLOAT32,floatDefault),
+	T3_Set_data(26,SN2_e11,dateType::FLOAT32,floatDefault),
+	T3_Set_data(27,SN2_e12,dateType::FLOAT32,floatDefault),
+	T3_Set_data(28,SN2_e13,dateType::FLOAT32,floatDefault),
+	T3_Set_data(29,SN2_e14,dateType::FLOAT32,floatDefault),
 	//sn3
-	T3_Set_data(SN3_e1,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e2,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e3,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e4,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e5,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e6,dateType::FLOAT32,floatDefault),
-	T3_Set_data(SN3_e7,dateType::BOOLTYPE,boolDefault),
+	T3_Set_data(30,SN3_e1,dateType::FLOAT32,floatDefault),
+	T3_Set_data(31,SN3_e2,dateType::FLOAT32,floatDefault),
+	T3_Set_data(32,SN3_e3,dateType::FLOAT32,floatDefault),
+	T3_Set_data(33,SN3_e4,dateType::FLOAT32,floatDefault),
+	T3_Set_data(34,SN3_e5,dateType::FLOAT32,floatDefault),
+	T3_Set_data(35,SN3_e6,dateType::FLOAT32,floatDefault),
+	T3_Set_data(36,SN3_e7,dateType::BOOLTYPE,boolDefault),
 	//零位
-	T3_Set_data(OLW_e1,dateType::FLOAT32,floatDefault),
-	T3_Set_data(OLW_e2,dateType::FLOAT32,floatDefault),
-	T3_Set_data(OLW_e3,dateType::FLOAT32,floatDefault),
-	T3_Set_data(OLW_e4,dateType::BOOLTYPE,boolDefault),
+	T3_Set_data(37,OLW_e1,dateType::FLOAT32,floatDefault),
+	T3_Set_data(38,OLW_e2,dateType::FLOAT32,floatDefault),
+	T3_Set_data(39,OLW_e3,dateType::FLOAT32,floatDefault),
+	T3_Set_data(40,OLW_e4,dateType::BOOLTYPE,boolDefault),
 };
+
+
 
 
 const CFG_INFO T4[] = {
-	T4_Set_data(AP1,dateType::STRING32,stingDefault),
+	T4_Set_data(1,M_AP1,dateType::STRING32,stingDefault),
+	T4_Set_data(2,B_AP2,dateType::STRING32,stingDefault),
+	T4_Set_data(3,D_AP0,dateType::STRING32,stingDefault),
+
+	T4_Set_data(4,CM_Group,dateType::INT32,int32Default),
+	T4_Set_data(5,CM_Times,dateType::INT32,int32Default),
+	T4_Set_data(6,CM_Cycle,dateType::INT32,int32Default),
+	T4_Set_data(7,AP_SS,dateType::INT32,int32Default),
+	T4_Set_data(8,Baudrate,dateType::INT32,int32Default),
+	T4_Set_data(9,TransmitPower,dateType::INT32,int32Default),
+
+	T4_Set_data(10,ServerIP,dateType::IP,IPServerDefault),
+	T4_Set_data(11,ServerPort,dateType::INT32,setDefault(int,8888)),
+	T4_Set_data(12,LocalIP,dateType::IP,IPLocalDefault),
+	T4_Set_data(13,Channel,dateType::INT32,setDefault(int,0)),
+	T4_Set_data(14,SinglePacketMax,dateType::INT32,int32Default),
 };
 
 const CFG_INFO T6[] = {
-	T6_Set_data(SN1_P1,dateType::INT32,stingDefault),
+	T6_Set_data(1,SN1_P1,dateType::INT32,setDefault(int,1200)),
+	T6_Set_data(2,SN1_P2,dateType::INT32,setDefault(int ,1600)),
+	T6_Set_data(3,SN1_P3,dateType::FLOAT32,setDefault(float ,0)),
+	T6_Set_data(4,SN1_P4,dateType::FLOAT32,setDefault(float ,0)),
+	T6_Set_data(5,SN1_P5,dateType::INT32,setDefault(int ,20)),//增益
+	T6_Set_data(6,SN1_P6,dateType::INT32,setDefault(int ,100)),//曝光
+	T6_Set_data(7,SN1_P7,dateType::BOOLTYPE,boolDefault),//水平翻转
+	T6_Set_data(8,SN1_P8,dateType::BOOLTYPE,boolDefault),//竖直翻转
+	T6_Set_data(9,SN1_P9,dateType::INT32,setDefault(int ,200)),//灰度阈值
+	T6_Set_data(10,SN1_P10,dateType::FLOAT32,setDefault(float ,0.8)),//二值化阈值
+	T6_Set_data(11,SN1_P11,dateType::FLOAT32,setDefault(float ,0.7)),//圆度阈值
+	T6_Set_data(12,SN1_P12,dateType::FLOAT32,setDefault(float ,0.8)),//上午有效百分比
+	T6_Set_data(13,SN1_P13,dateType::FLOAT32,setDefault(float ,0.8)),//下午有效百分比
+	T6_Set_data(14,SN1_P14,dateType::FLOAT32,setDefault(float ,20)),//太阳高度角阈值
+	T6_Set_data(15,SN1_P15,dateType::INT32,boolDefault),//主动修正标志  bool
+	T6_Set_data(16,SN1_P16,dateType::INT32,boolDefault),//校正标志位 bool
+	T6_Set_data(17,SN1_P17,dateType::INT32,setDefault(int ,10)),//采集间隔时间
+	T6_Set_data(18,SN1_P18,dateType::FLOAT32,setDefault(float ,0.95)),//1mrad
+	T6_Set_data(19,SN1_P19,dateType::FLOAT32,setDefault(float ,0.95)),//2mrad
+
+
+	T6_Set_data(19,SN2_P1,dateType::FLOAT32,floatDefault),//目标sn3相机坐标x
+	T6_Set_data(20,SN2_P2,dateType::FLOAT32,floatDefault),//目标sn3相机坐标y
+	T6_Set_data(21,SN2_P3,dateType::FLOAT32,floatDefault),//目标sn3相机坐标z
+	T6_Set_data(22,SN2_P4,dateType::INT32,setDefault(float ,0.95)),//SN2用T8表频率(行/秒)
+	T6_Set_data(23,SN2_P5,dateType::INT32,setDefault(float ,0.95)),//SN2用最大转角速度（度/秒）
+	T6_Set_data(24,SN2_P6,dateType::INT32,setDefault(float ,0.95)),//SN2用法线最大扫描距离（米）
+
+	T6_Set_data(25,SaveOrg,dateType::BOOLTYPE,boolDefault),
+	T6_Set_data(26,SaveBin,dateType::BOOLTYPE,boolDefault),
+	T6_Set_data(27,SavePath,dateType::STRING64,stingDefault),
 };
+
 
 const CFG_GROUP cfg_group[] = {
 	Set_Group(T1,0),
