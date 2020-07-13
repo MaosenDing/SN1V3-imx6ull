090a37157b5ed72dea2358baeba777cfd862e1d9
ririyeye <200610237@qq.com>
2020-07-13 14:03:24 +0800

    添加cmake toolchain

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ fi
 
 mkdir ${build}
 cd ${build}
-cmake .. -DCMAKE_INSTALL_PREFIX=${workPath} -Dver=${ver}
+cmake .. -DCMAKE_INSTALL_PREFIX=${workPath} -Dver=${ver} -DCMAKE_TOOLCHAIN_FILE=../compiler.cmake
 cpunum=$(cat /proc/cpuinfo | grep processor | wc -l)
 make -j${cpunum}
 sudo make install/strip
